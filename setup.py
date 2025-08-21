1e41ee387e74a983de4c6d6733625ac49e0006bd
juanQNav <juan.qn363@gmail.com>
2025-08-21 14:33:21 -0600

    add: setup.py to install with pip

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ingest_ragflow",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests",
+        "tqdm",
+        "pandas",
+        "ragflow-sdk",
+    ],
+    python_requires=">=3.13",
+)
