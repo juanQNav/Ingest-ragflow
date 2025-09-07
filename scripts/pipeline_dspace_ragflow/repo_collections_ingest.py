e65ec30d45f9112e1caf161e22cee589d48ef3ad
juanQNav <juan.qn363@gmail.com>
2025-09-06 21:16:44 -0600

    feature: update scripts

---
+++ scripts/pipeline_dspace_ragflow/repo_collections_ingest.py
@@ -0,0 +1,102 @@
+import argparse
+import asyncio
+import os
+
+from ragflow_sdk import RAGFlow
+from tqdm import tqdm
+
+from ingest_ragflow.dspace_api.collections import get_collections, select_collection
+from ingest_ragflow.rag.parsing import (
+    monitor_parsing,
+    process_collections_in_parallel,
+)
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "--folder_path", required=True, help="Folder path for downloaded files"
+    )
+    ap.add_argument("--apikey", required=True, help="API key for RAGFlow")
+    ap.add_argument("--ragflow_url", required=True, help="RAGFlow URL")
+    ap.add_argument(
+        "--max_tasks", default=8, type=int, help="Maximum number of concurrent tasks"
+    )
+
+    args = vars(ap.parse_args())
+
+    # Configuration
+    BASE_URL = "https://repositorioinstitucional.uaslp.mx/"
+    BASE_URL_REST = "https://repositorioinstitucional.uaslp.mx/rest"
+    FOLDER_PATH = args["folder_path"]
+    MAX_CONCURRENT_TASKS = args["max_tasks"]
+
+    # Create output directory if it does not exist
+    os.makedirs(FOLDER_PATH, exist_ok=True)
+
+    print("Getting collections...")
+    collections_ids = get_collections(BASE_URL_REST, verbose=True)
+    if collections_ids is not None:
+        print(f"Found {len(collections_ids)} collections.")
+
+        # Initialize RAGFlow
+        API_KEY = args["apikey"]
+        RAG_URL = args["ragflow_url"]
+        rag_object = RAGFlow(api_key=API_KEY, base_url=RAG_URL)
+
+        # Choose or create dataset
+        create_new_dataset = input("Create new dataset? (y/n): ").strip().lower() == "y"
+
+        if create_new_dataset:
+            dataset = rag_object.create_dataset(name="test_knowledge")
+            dataset.update(
+                {
+                    "embedding_model": "mxbai-embed-large:latest",
+                    "chunk_method": "naive",
+                    "parser_config": {
+                        "chunk_token_count": 128,
+                        "delimiter": "\n!?;。；！？",
+                        "layout_recognize": True,
+                        "html4excel": False,
+                        "raptor": {"use_raptor": False},
+                    },
+                }
+            )
+        else:
+            datasets = rag_object.list_datasets()
+            for i, dataset in enumerate(datasets):
+                print(f"{i}: {dataset.name}")
+            selected_index = int(input("Enter dataset ID: "))
+            dataset = rag_object.list_datasets(id=datasets[selected_index].id)[0]
+
+        # List of documents
+        document_ids = []
+        collections_ids = [select_collection(collections_ids)]
+
+        # Retrieve items from collections and process them in parallel
+        process_collections_in_parallel(
+            base_url=BASE_URL,
+            base_url_rest=BASE_URL_REST,
+            collections_ids=collections_ids,
+            folder_path=FOLDER_PATH,
+            ragflow_dataset=dataset,
+            document_ids=document_ids,
+            max_concurrent_tasks=MAX_CONCURRENT_TASKS,
+        )
+
+        # Monitoring after downloading
+        tqdm.write("Starting document parsing monitoring...")
+        asyncio.run(monitor_parsing(dataset, document_ids))
+
+        # Final document status
+        documents = dataset.list_documents()
+        print("\nFinal Summary:")
+        print("-" * 50)
+        for doc in documents:
+            print(
+                f"{doc.name} | Status: {doc.run} |\
+                Fragments: {doc.chunk_count}"
+            )
+        print("-" * 50)
+        print("Process completed successfully.")
+    else:
+        print("No collections found.")
