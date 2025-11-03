ab40505d91efa5ab874dde431d3afc49a2cf4508
juanQNav <juan.qn363@gmail.com>
2025-11-03 11:31:15 -0600

    chore(dependencies): add dependencies flake8, coverage and pytest

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="ingest_ragflow",
     version="0.1.0",
+    description="This repository contains code for integrating and using various components, including the DSpace API and a knowledge base in Ragflow.",
+    author="juanQNav et al.",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
@@ -12,4 +14,12 @@ setup(
         "ragflow-sdk",
     ],
     python_requires=">=3.10",
+    extras_require={
+        "dev": [
+            "ruff>=0.13.2",
+            "flake8>=7.1.1,<7.2",
+            "coverage>=7.10,<7.11",
+            "pytest>=8.4.1",
+        ],
+    },
 )
